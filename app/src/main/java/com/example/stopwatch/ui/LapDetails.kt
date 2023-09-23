fef94a9bc5eaba6bc2c5c6079d67f4dcc4b8cbf1
Zeyad Abdullah <zeyadgalal93@gmail.com>
2023-09-23 17:19:27 +0300

    update lab details

--- app/src/main/java/com/example/stopwatch/ui/LapDetails.kt
+++ app/src/main/java/com/example/stopwatch/ui/LapDetails.kt
@@ -1,12 +1,18 @@
 package com.example.stopwatch.ui
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -20,30 +26,39 @@ fun LapDetails(
 ) {
     Row(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
+            .fillMaxWidth(),
     ) {
         ItemTextHeader(type = "Lap", modifier = Modifier.weight(1f))
         ItemTextHeader(type = "Lap Time", modifier = Modifier.weight(1f))
         ItemTextHeader(type = "Overall", modifier = Modifier.weight(1f))
     }
+    Spacer(modifier = Modifier.height(8.dp))
     LazyColumn(
         modifier
             .fillMaxWidth()
-            .padding(start = 8.dp, end = 8.dp),
     ) {
         items(lapTime.size) { index ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 16.dp),
+                    .padding(vertical = 8.dp)
             ) {
-                Text(
-                    text = lapCount[index].toString(),
-                    style = MaterialTheme.typography.titleSmall,
-                    modifier = Modifier.weight(1f),
-                    textAlign = TextAlign.Center
-                )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    val circleColor = MaterialTheme.colorScheme.primaryContainer
+                    Canvas(modifier = Modifier.size(24.dp), onDraw = {
+                        drawCircle(color = circleColor)
+                    })
+                    Text(
+                        text = lapCount[index].toString(),
+                        style = MaterialTheme.typography.titleSmall,
+                        modifier = Modifier,
+                        textAlign = TextAlign.Center
+                    )
+                }
                 Text(
                     text = formatMilliseconds(lapTime[index]).toString()
                         .drop(1).dropLast(1)
